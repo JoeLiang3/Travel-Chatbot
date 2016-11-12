02f1c551ab00c95bdead3a80d2130affb3267fc2
Janesh Chhabra <janeshchhabra@gmail.com>
2016-11-12 08:24:59 -0800

    finished ai for plane cheap prices :

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var max_price = 500000;
 var numberResults = 5;
 var originBool = true;
 var departureBool = true;
+var loadMore = 0
 $(document).ready(function () { // start js once document loads
 
  
@@ -61,9 +62,31 @@ function Chat (input){
     sendMessage(message);
     departureBool = !departureBool
     var date = input.split("/");
-    var departureDate = "20" + input[2] + "-" + input[0] + "-" + input[1] + "T23%3A59";
-    returnsBy = returnsBy;
-    getPrice();
+    var returnDate = "20" + input[2] + "-" + input[0] + "-" + input[1] + "T23%3A59";
+    returnsBy = returnDate;
+    getPrices();
   }
+  else if (input.contains("yes") || input.contains("Yes")){
+    var message = "Here are more results!"
+    sendMessage(message);
+    loadMore += 1;
+    getPrices();
+  }
+}
+function getPrices(){
+    
+  var beginURL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search/v1.2/flights/low-fare-search?    apikey=NNd0iA0mK7NWdWJreIepC8Heb41azLry";
+  var URL = beginURL + "&origin=" + origin + "&destination=" + destination + "&departure_date=" + departure
+            + "&return_by=" + returnBy + "&adults=" + adults + "&children=" + children + "&max_price=" +
+            max_price + "&number_of_results=" + numberResults + " HTTP/1.1";
   
+  
+  $.getJSON(URL, function(data) {
+    console.log(data);
+    console.log(data.results[1].fare.total_price);
+    var message = "Here is the cheapest plane ticket fitting your criteria:" + data.results[loadMore].fare.total_price;
+    sendMessage(message);
+    message = "Do you want to load more results?"
+    sendMessage(message);
+  });
 }
