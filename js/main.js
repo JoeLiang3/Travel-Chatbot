2b92def7b16dc943aebe865a31854564f105f7ce
Janesh Chhabra <janeshchhabra@gmail.com>
2016-11-12 07:27:39 -0800

    Done with core AI.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
+var origin = "SFO"
+var destination = "LAX"
+var departure = "2016-11-25"
+var returnBy = "2016-12-1T23%3A59"
+var adults = 1
+var children = 0
+var max_price = 500000
+var numberResults = 5
+var originBool = true
+var departureBool = true
 $(document).ready(function(){ // start js once document loads
-  var origin = "SFO"
-  var destination = "LAX"
-  var departure = "2016-11-25"
-  var returnBy = "2016-12-1T23%3A59"
-  var adults = 1
-  var children = 0
-  var max_price = 500000
-  var numberResults = 5
+
  
   var beginURL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search/v1.2/flights/low-fare-search?apikey=NNd0iA0mK7NWdWJreIepC8Heb41azLry"
   var URL = beginURL + "&origin=" + origin + "&destination=" + destination + "&departure_date=" + departure
@@ -22,3 +25,44 @@ $(document).ready(function(){ // start js once document loads
     }
   });
 });
+
+function Chat (input){
+  if (input == ""){
+    var message = "Hi, welcome to flight tracker. Please tell us the code of the departure airport.";
+    sendMessage(message);
+  }
+  else if (input.length == 3 && originBool){
+    var message = "Thank you. Now please tell us the code of the arrival airport.";
+    originBool = !originBool;
+    sendMessage(message);
+    origin = input; 
+  }
+  else if (input.length == 3 && !originBool){
+    var message = "How many adults are you looking to have?";
+    originBool = !originBool;
+    sendMessage(message);
+    destination = input; 
+  }
+  else if (input.contains("adults")){
+    var message = "When are you planning to leave?";
+    sendMessage(message);
+    adults = input;
+  }
+  else if (input.contains("/") && departureBool){
+    var message = "When are you planning to return by?";
+    sendMessage(message);
+    departureBool = !departureBool
+    var date = input.split("/");
+    var departureDate = "20" + input[2] + "-" + input[0] + "-" + input[1];
+    departure = departureDate;
+  }
+  else if (input.contains("/") && !departureBool){
+    var message = "When are you planning to return?";
+    sendMessage(message);
+    departureBool = !departureBool
+    var date = input.split("/");
+    var departureDate = "20" + input[2] + "-" + input[0] + "-" + input[1] + "T23%3A59";
+    returnsBy = returnsBy;
+  }
+  
+}
