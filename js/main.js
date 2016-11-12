ebbda271d8bd510a5c2c91b2720d19dbb338775c
Janesh Chhabra <janeshchhabra@gmail.com>
2016-11-12 07:38:34 -0800

    fixed technical issues

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-var origin = "SFO"
-var destination = "LAX"
-var departure = "2016-11-25"
-var returnBy = "2016-12-1T23%3A59"
-var adults = 1
-var children = 0
-var max_price = 500000
-var numberResults = 5
-var originBool = true
-var departureBool = true
-$(document).ready(function(){ // start js once document loads
+var origin = "SFO";
+var destination = "LAX";
+var departure = "2016-11-25";
+var returnBy = "2016-12-1T23%3A59";
+var adults = 1;
+var children = 0;
+var max_price = 500000;
+var numberResults = 5;
+var originBool = true;
+var departureBool = true;
+$(document).ready(function () { // start js once document loads
 
  
-  var beginURL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search/v1.2/flights/low-fare-search?apikey=NNd0iA0mK7NWdWJreIepC8Heb41azLry"
+  var beginURL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search/v1.2/flights/low-fare-search?apikey=NNd0iA0mK7NWdWJreIepC8Heb41azLry";
   var URL = beginURL + "&origin=" + origin + "&destination=" + destination + "&departure_date=" + departure
             + "&return_by=" + returnBy + "&adults=" + adults + "&children=" + children + "&max_price=" +
             max_price + "&number_of_results=" + numberResults + " HTTP/1.1";
